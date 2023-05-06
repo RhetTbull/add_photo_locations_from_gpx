43659dfb1f87d5148820658710a2a85b8f05a096
Rhet Turnbull <rturnbull@gmail.com>
2023-05-06 09:04:57 -0700

    Added --offset

--- add_photo_locations_from_gpx.py
+++ add_photo_locations_from_gpx.py
@@ -1,24 +1,47 @@
 """ Add missing location information to photos in Apple Photos from a GPX file """
 
+from __future__ import annotations
 
 import sys
 from bisect import bisect_left
-from datetime import datetime
+from datetime import datetime, timedelta
 from functools import cached_property
+from textwrap import dedent
 from typing import Optional
 
 import click
 import gpxpy
 from gpxpy.gpx import GPXTrackPoint
 from osxphotos import PhotoInfo, PhotosDB
+from osxphotos.cli.param_types import TimeOffset
+from osxphotos.datetime_utils import (
+    datetime_naive_to_utc,
+    datetime_remove_tz,
+    datetime_utc_to_local,
+)
 from osxphotos.photosalbum import PhotosAlbum
 from osxphotos.utils import pluralize
 from photoscript import PhotosLibrary
+from rich import print
 
 DEFAULT_TIME_DELTA = 60
 LOCATION_HISTORY = None
 
 __version__ = "0.2.0"
+
+
+def gpx_datetime_to_local(dt: datetime) -> datetime:
+    """Convert a GPX datetime to local datetime
+
+    GPX datetime is UTC but may have timezone SimpleTZ("Z")
+
+    Args:
+        dt: timezone aware datetime.datetime object in UTC
+
+    Returns:
+        timezone aware datetime.datetime object in local timezone
+    """
+    return datetime_utc_to_local(datetime_naive_to_utc(datetime_remove_tz(dt)))
 
 
 class GPXData:
@@ -72,17 +95,25 @@ def add_location_to_photo(
     photo: PhotoInfo,
     gpx_data: GPXData,
     delta: int,
+    offset: timedelta | None,
     dry_run: bool,
     album: Optional[PhotosAlbum] = None,
 ) -> int:
     """Add location information to photo record, returns 1 if location added, else 0"""
-    nearest_location, nearest_delta = gpx_data.nearest_location(photo.date)
+    date = photo.date + offset if offset is not None else photo.date
+    nearest_location, nearest_delta = gpx_data.nearest_location(date)
     nearest_delta = int(nearest_delta)
+
     if nearest_delta >= delta:
         return 0
 
-    click.echo(
-        f"Found location match for {photo.original_filename} taken on {photo.date} "
+    offset_str = (
+        f" (offset by {offset.total_seconds()} seconds = {date})"
+        if offset is not None
+        else ""
+    )
+    print(
+        f"Found location match for {photo.original_filename} taken on {photo.date}{offset_str} "
         f"within {nearest_delta} seconds: "
         f"{nearest_location.latitude}, {nearest_location.longitude}"
     )
@@ -93,17 +124,17 @@ def add_location_to_photo(
             if library_photo:
                 library_photo = list(library_photo)[0]
             else:
-                click.echo(f"Error: could not access photo for uuid {photo.uuid}")
+                print(f"Error: could not access photo for uuid {photo.uuid}")
                 return 0
             library_photo.location = (
                 nearest_location.latitude,
                 nearest_location.longitude,
             )
-            click.echo("Added location to photo")
+            print("Added location to photo")
             if album:
                 album.add(photo)
         except Exception as e:
-            click.echo(f"Error: could not add location to photo {e}")
+            print(f"Error: could not add location to photo {e}")
             return 0
     return 1
 
@@ -115,6 +146,29 @@ def add_location_to_photo(
     type=int,
     default=DEFAULT_TIME_DELTA,
     help=f"Time delta in seconds, default = {DEFAULT_TIME_DELTA}.",
+)
+@click.option(
+    "--offset",
+    type=TimeOffset(),
+    help=dedent(
+        """Time offset of your photos from the timestamp in the GPX file.
+    Date/time stamps in the GPX file are specified in UTC timezone.
+    If you import a photo without a timezone into Photos, Photos will assume the photo is in your local timezone
+    at the time of the import. This script will use the timezone information in the Photos library to correctly
+    compare to the UTC date/time in the GPX file. However, if the datetime information in the Photos library is
+    not correct, you can specify an offset to shift the date/time for comparison. For example, if you were in
+    UTC timezone when you took the photos but then imported the photos into Photos while you were in the EST
+    timezone, Photos would assume the photos were taken in EST timezone and the Photos date/time would actually
+    be 5 hours earlier than the UTC date/time in the GPX file.
+    --offset may thus be required to shift the date/time in Photos to match the GPX date/time.
+    For example, if your local timezone is UTC-5 (EST) you can specify "--offset '-5 hours'" to shift the date/time
+    in Photos by 5 hours before comparing to the GPX file.
+    The offset time can be specified in a number of formats, for example "--offset -05:00:00" which is in (HH:MM:SS)
+    format. If your timezone is ahead of UTC, you would use a positive offset, for example --offset +02:00:00.
+    This option may also be useful if the camera clock was off by a certain amount of time.
+    Valid format for date/time offset: '±HH:MM:SS', '±H hours' (or hr), '±M minutes' (or min), '±S seconds' (or sec), '±S' (where S is seconds)
+    """
+    ),
 )
 @click.option(
     "--dry-run",
@@ -132,31 +186,36 @@ def add_location_to_photo(
     help="Only update photos currently selected in Photos. "
     "Default is to update all photos with missing location data in the library.",
 )
-def main(gpx_filename, delta, dry_run, add_to_album, selected):
+def main(gpx_filename, delta, offset, dry_run, add_to_album, selected):
     """Add missing location data to photos in Apple Photos from GPX file."""
-    click.echo(f"Version: {__version__}")
-    click.echo(f"Loading GPX data from {gpx_filename}")
+    print(f"Version: {__version__}")
+    print(f"Offset: {offset}")
+    print(f"Loading GPX data from {gpx_filename}")
     gpx_data = GPXData(gpx_filename)
     if len(gpx_data.points) == 0:
-        click.echo("No tracks found in GPX file.", err=True)
+        print("No tracks found in GPX file.", err=True)
         sys.exit(1)
-    click.echo(
+    print(
         f"Loaded {len(gpx_data.points)} {pluralize(len(gpx_data.points), 'track point', 'track points')} from GPX file"
     )
 
     earliest = gpx_data.points[0]
     latest = gpx_data.points[-1]
-    click.echo(f"Earliest: {earliest.time}, {earliest.latitude}, {earliest.longitude}")
-    click.echo(f"Latest: {latest.time}, {latest.latitude}, {latest.longitude}")
+    print(
+        f"Earliest: {earliest.time} ({gpx_datetime_to_local(earliest.time)}), {earliest.latitude}, {earliest.longitude}"
+    )
+    print(
+        f"Latest: {latest.time} ({gpx_datetime_to_local(latest.time)}), {latest.latitude}, {latest.longitude}"
+    )
 
-    click.echo("Loading photo library")
+    print("Loading photo library")
     photosdb = PhotosDB()
-    click.echo(f"Loaded {len(photosdb)} {pluralize(len(photosdb), 'photo', 'photos')}")
+    print(f"Loaded {len(photosdb)} {pluralize(len(photosdb), 'photo', 'photos')}")
 
     if selected:
         selected_photos = PhotosLibrary().selection
         if not selected_photos:
-            click.echo("No photos selected in Photos.", err=True)
+            print("No photos selected in Photos.", err=True)
             sys.exit(1)
         photos = [
             photo
@@ -170,21 +229,21 @@ def main(gpx_filename, delta, dry_run, add_to_album, selected):
             if photo.location == (None, None) and not photo.shared
         ]
 
-    album = PhotosAlbum(add_to_album, verbose=click.echo) if add_to_album else None
-    click.echo(
+    album = PhotosAlbum(add_to_album, verbose=print) if add_to_album else None
+    print(
         f"Checking {len(photos)} {pluralize(len(photos), 'photo', 'photos')} "
         f"that lack{'s' if len(photos)==1 else ''} location information"
     )
     results = sum(
-        add_location_to_photo(photo, gpx_data, delta, dry_run, album=album)
+        add_location_to_photo(photo, gpx_data, delta, offset, dry_run, album=album)
         for photo in photos
     )
 
-    click.echo(
+    print(
         f"Added location info to {results} {pluralize(results, 'photo', 'photos')}"
     )
     if add_to_album:
-        click.echo(f"Added to album '{add_to_album}'")
+        print(f"Added to album '{add_to_album}'")
 
 
 if __name__ == "__main__":
